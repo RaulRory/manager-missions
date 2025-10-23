f3d8d2b51984b1eeaa7ff22116489b200f69d2a5
Raul Rory <raulrory@gmail.com>
2025-10-23 02:33:14 -0300

    feat: create config datatabse

--- src/database/config.js
+++ src/database/config.js
@@ -1,3 +1,25 @@
 import { DatabaseSync } from "node:sqlite";
 
 export const database = new DatabaseSync(":memory");
+
+const queryMission =`
+CREATE TABLE mission(
+    id text PRIMARY KEY NOT NULL,
+    name text NOT NULL,
+    crew text NOT NULL,
+    spacecraft text NOT NULL,
+    destination text NOT NULL,
+    status text NOT NULL,
+    duration integer NOT NULL 
+)`
+
+function createTableMission() {
+    return database.exec(queryMission);
+}
+
+export function dbInitialization() {
+    console.info("Table Mission is send to create");
+    createTableMission();
+    console.info("Table Mission is created with success");
+    return;
+}
